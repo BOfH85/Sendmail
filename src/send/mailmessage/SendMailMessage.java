ec5c73586fb3dfa0f8a726483aff5786d66f7f12
jungoliver <jungoliver@IT010741.intra.schwaebischer-verlag.de>
2018-02-13 11:29:38 +0100

    12.02.2018

---
+++ src/send/mailmessage/SendMailMessage.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package send.mailmessage;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author jungoliver
+ */
+public class SendMailMessage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnsupportedEncodingException, MessagingException {
+        
+        //Get Arguments
+        String from = ""; 
+        String to = "";
+        String bcc;
+        String cc; 
+        String subject = "";
+        String body = "";
+        
+        for (int i=0;i<args.length;i++)
+        {
+            switch(args[i])
+            {
+                case "-from":
+                    from =args[i+1];
+                    break;
+                case "-to":
+                    to =args[i+1];
+                    break;
+                case "-cc":
+                    cc =args[i+1];
+                    break;    
+                case "-bcc":
+                    bcc =args[i+1];
+                    break;  
+                case "-subject":
+                    subject =args[i+1];
+                    break;
+                case "-body":
+                    body =args[i+1];
+                    break;    
+            }
+        }
+        
+        if (!(from.isEmpty())&& !(to.isEmpty()))
+        {
+        final Properties props = System.getProperties();
+               
+        props.put("mail.smtp.host", "smtp.intra.schwaebischer-verlag.de");
+        props.put("mail.smtp.port", "");
+        props.put("mail.transport.protocol","smtp");
+
+        Session session = Session.getDefaultInstance(props);
+
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress((from)));
+        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        //msg.addRecipient(Message.RecipientType.TO, new InternetAddress("msg_vertrieb@msgmediaservice.de"));
+        
+        msg.setSubject(subject);
+        msg.setText(body);
+        msg.saveChanges();
+        Transport.send(msg);
+        }
+    }
+    
+}
